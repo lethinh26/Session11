d0aa5f903789ed7e7c75f96f7df1b0978c0f6cf2
lethinh <68421373+lethinh26@users.noreply.github.com>
2024-12-30 10:15:32 +0700

    Update Session11BT4.cpp

--- Session11BT4.cpp
+++ Session11BT4.cpp
@@ -1,15 +1,14 @@
 #include <stdio.h>
-int arrayInt[] = {1,2,3,4,5};
-int size = sizeof(arrayInt)/sizeof(int);
-
-int maxArray();
+int maxArray(int arrayInt[], int size);
 
 int main() {
-    printf("Gia tri lon nhat cua mang la: %d", maxArray());
+    int arrayInt[] = {1,2,3,4,5};
+    int size = sizeof(arrayInt)/sizeof(int);
+    printf("Gia tri lon nhat cua mang la: %d", maxArray(arrayInt, size));
     return 0;
 }
 
-int maxArray() {
+int maxArray(int arrayInt[], int size) {
     int max = arrayInt[0];
     for (int i = 0; i < size; i++) {
         if (max < arrayInt[i]) {
